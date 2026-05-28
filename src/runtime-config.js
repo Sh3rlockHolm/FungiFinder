a829f1eb2d942eda55a5d5cd28786db0c4f90a8d
Sh3rlockHolm <holm.roeser@gmail.com>
2026-05-28 10:21:54 +0200

    Configure production feedback endpoint for shared Google Apps Script logging

--- src/runtime-config.js
+++ src/runtime-config.js
@@ -2,6 +2,8 @@
 // Set these values in production to enable shared, remote feedback logging.
 // Example:
 // window.FUNGI_FEEDBACK_ENDPOINT = "https://script.google.com/macros/s/AKfycb.../exec";
-window.FUNGI_FEEDBACK_ENDPOINT = window.FUNGI_FEEDBACK_ENDPOINT || "";
+window.FUNGI_FEEDBACK_ENDPOINT =
+  window.FUNGI_FEEDBACK_ENDPOINT ||
+  "https://script.google.com/macros/s/AKfycbwbvr8rT9tdGsWW9GAwApi5ewELW0rRJNCjHPRxknL09I--x_izpioXX8qxWY_x2gVu/exec";
 window.FUNGI_FEEDBACK_API_KEY = window.FUNGI_FEEDBACK_API_KEY || "";
 window.FUNGI_FEEDBACK_API_KEY_HEADER = window.FUNGI_FEEDBACK_API_KEY_HEADER || "x-api-key";
