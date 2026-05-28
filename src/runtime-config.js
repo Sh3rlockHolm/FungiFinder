996574e5e570e9c17b8bd71ff8b9174d5c34ffe1
Sh3rlockHolm <holm.roeser@gmail.com>
2026-05-28 12:50:26 +0200

    Configure production feedback API key for Apps Script sync

--- src/runtime-config.js
+++ src/runtime-config.js
@@ -5,5 +5,7 @@
 window.FUNGI_FEEDBACK_ENDPOINT =
   window.FUNGI_FEEDBACK_ENDPOINT ||
   "https://script.google.com/macros/s/AKfycbwbvr8rT9tdGsWW9GAwApi5ewELW0rRJNCjHPRxknL09I--x_izpioXX8qxWY_x2gVu/exec";
-window.FUNGI_FEEDBACK_API_KEY = window.FUNGI_FEEDBACK_API_KEY || "";
+window.FUNGI_FEEDBACK_API_KEY =
+  window.FUNGI_FEEDBACK_API_KEY ||
+  "6bn+dytyr8WpItnu5H7LVHf0biLSAO9Zf7+Oe5Svv8PAXrCcfs6DD4m4hLF74JCN";
 window.FUNGI_FEEDBACK_API_KEY_HEADER = window.FUNGI_FEEDBACK_API_KEY_HEADER || "x-api-key";
